71771230f29819e060a270d07a6b4d0224eb30e0
antoniovicenzo <149817161+antoniovicenzo@users.noreply.github.com>
2025-05-04 10:39:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,20 @@ import streamlit as st
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
-from torchvision.models import resnet18
-import torch.nn as nn
 
 # Load model
-model = resnet18(pretrained=False)
-model.fc = nn.Linear(model.fc.in_features, 2)
-model.load_state_dict(torch.load("models/fake_art_classifier.pt", map_location=torch.device('cpu')))
+model = torch.load("models/fake_art_classifier_full.pt", map_location=torch.device('cpu'))
 model.eval()
 
-# Transform input image
+# Transform
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
 ])
 
-st.title("🎨 Fake Art Detector")
+labels = ["Real Artwork", "AI-Generated"]
+
+st.title("\U0001F3A8 Fake Art Detector")
 image_file = st.file_uploader("Upload an image to check:", type=["jpg", "png"])
 
 if image_file:
@@ -29,7 +27,7 @@ if image_file:
     confidence = torch.softmax(output, dim=1)
     class_idx = torch.argmax(confidence).item()
 
-    label = "Real Artwork" if class_idx == 0 else "AI-Generated"
+    label = labels[class_idx]
     confidence_score = confidence[0][class_idx].item() * 100
 
     st.markdown(f"### Result: {label} ({confidence_score:.2f}% confidence)")
